77d4a8e6a1f16f63eae12314b1220e2282776591
jDmacD <jmacdoo@gmail.com>
2025-06-09 11:31:43 +0100

    update agentconfig

--- nix/modules/nixos/k3s-agent.nix
+++ nix/modules/nixos/k3s-agent.nix
@@ -22,11 +22,11 @@
 
   services.k3s = {
     enable = true;
-    package = pkgs.unstable.k3s_1_30;
+    package = pkgs.k3s_1_33;
     role = "agent"; # Or "agent" for worker only nodes
     # sudo cat /var/lib/rancher/k3s/server/agent-token
-    token = "K10d3eae50e388e04587ce0188128e9b04b4b92a2f6aa0e14981483dc6e1e5fbe26::server:b1dbb3ed45a79cbb4f0c20c87c01b8b5";
-    serverAddr = "https://tpi01.lan:6443";
+    token = "<server token>";
+    serverAddr = "https://<server>:6443";
     extraFlags = toString [
       # otherwise the node will pull the nixos-rpi hostname of the bootstapper
       "--node-name ${config.networking.hostName}"
