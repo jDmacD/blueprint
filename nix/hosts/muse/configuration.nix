157221bfc2dd1cf2ae10394a0c2974fec51bfc01
jDmacD <jmacdoo@gmail.com>
2025-06-09 11:09:41 +0100

    comments

--- nix/hosts/muse/configuration.nix
+++ nix/hosts/muse/configuration.nix
@@ -8,6 +8,8 @@
 {
   imports = [
     ./hardware-configuration.nix
+    # Because we're pulling this into default.nix,
+    # we need to use relative paths otherwise it causes a recursion error.
     ../../modules/nixos/host-shared.nix
     ../../modules/nixos/k3s-server.nix
   ];
