8c33945a968cb804dab16a17b7ee0fc561abf24a
jDmacD <jmacdoo@gmail.com>
2025-12-14 10:44:24 +0000

    add kernel patches

--- nix/hosts/pi01/configuration.nix
+++ nix/hosts/pi01/configuration.nix
@@ -36,6 +36,18 @@
       "cgroup_memory=1"
       "cgroup_enable=memory"
     ];
+    kernelPatches = [
+      {
+        name = "custom-kernel-config";
+        patch = null;
+        extraConfig = ''
+          ARM64_VA_BITS_47 n
+          ARM64_VA_BITS_48 y
+          ARM64_VA_BITS 48
+          PGTABLE_LEVELS 4
+        '';
+      }
+    ];
   };
 
   sops.defaultSopsFile = ../secrets.yaml;
