23a4594cd45ef27666d013a8f7454c45c6892a6a
james.macdonald <james.macdonald@heanet.ie>
2025-12-15 15:36:13 +0000

    alternate approach

--- nix/modules/nixos/sops.nix
+++ nix/modules/nixos/sops.nix
@@ -1,11 +1,24 @@
 { pkgs, inputs, lib, ... }:
 
 {
-  # Only import the appropriate module based on the system type
-  imports = lib.optional (pkgs.stdenv.hostPlatform.isLinux) inputs.sops-nix.nixosModules.sops
-         ++ lib.optional (pkgs.stdenv.hostPlatform.isDarwin) inputs.sops-nix.darwinModules.sops;
+  # Import both modules unconditionally
+  imports = [
+    inputs.sops-nix.nixosModules.sops
+    inputs.sops-nix.darwinModules.sops
+  ];
   
   # Configure sops
   sops.defaultSopsFile = ../secrets.yaml;
   sops.defaultSopsFormat = "yaml";
+  
+  # Conditionally enable the appropriate module's functionality
+  # This works because each module's implementation will check if it's on the right platform
+  # and not activate if it's on the wrong one
+  nixos.sops = lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
+    enable = true;
+  };
+  
+  darwin.sops = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
+    enable = true;
+  };
 }
