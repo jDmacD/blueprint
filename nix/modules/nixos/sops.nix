0b90919c9f9f10111d7315b383ffa488aed7c5a2
jDmacD <jmacdoo@gmail.com>
2025-12-15 15:44:28 +0000

    remove darwin import

--- nix/modules/nixos/sops.nix
+++ nix/modules/nixos/sops.nix
@@ -1,24 +1,13 @@
-{ pkgs, inputs, lib, ... }:
+{ inputs, ... }:
 
 {
-  # Import both modules unconditionally
+  # Import only the NixOS sops module
+  # Darwin hosts should import inputs.sops-nix.darwinModules.sops directly in their configuration
   imports = [
     inputs.sops-nix.nixosModules.sops
-    inputs.sops-nix.darwinModules.sops
   ];
-  
+
   # Configure sops
   sops.defaultSopsFile = ../secrets.yaml;
   sops.defaultSopsFormat = "yaml";
-  
-  # Conditionally enable the appropriate module's functionality
-  # This works because each module's implementation will check if it's on the right platform
-  # and not activate if it's on the wrong one
-  nixos.sops = lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
-    enable = true;
-  };
-  
-  darwin.sops = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
-    enable = true;
-  };
 }
