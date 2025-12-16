56ef9565168f1ceebe4b850c32dd48f950522454
james.macdonald <james.macdonald@heanet.ie>
2025-12-16 16:53:27 +0000

    skip lore

--- flake.nix
+++ flake.nix
@@ -108,11 +108,11 @@
               arch = "x86_64-linux";
               remoteBuild = true;
             };
-            lore = mkNode {
-              name = "lore";
-              arch = "aarch64-darwin";
-              remoteBuild = true;
-            };
+            # lore = mkNode {
+            #   name = "lore";
+            #   arch = "aarch64-darwin";
+            #   remoteBuild = true;
+            # };
             worf = mkNode {
               name = "worf";
               hostname = "worf.jtec.xyz";
