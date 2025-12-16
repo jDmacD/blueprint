5e892de365c8f2d1c155f9abab5e719b5e9a0db6
jDmacD <jmacdoo@gmail.com>
2025-12-16 13:16:12 +0000

    update pi01 config and readme

--- nix/hosts/pi01/configuration.nix
+++ nix/hosts/pi01/configuration.nix
@@ -9,13 +9,13 @@
   imports = with inputs.self.nixosModules; [
     rpi-common
     k3s-agent
-    sd-image-config
-    rpi-minimal-firmware
+    # sd-image-config
+    # rpi-minimal-firmware
   ];
 
   # Enable minimal firmware to save ~1.3MB on /boot/firmware
-  boot.loader.raspberryPi.useMinimalFirmware = true;
+  # boot.loader.raspberryPi.useMinimalFirmware = false;
 
   networking.hostName = "pi01";
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "25.11"; # Did you read the comment?
 }
