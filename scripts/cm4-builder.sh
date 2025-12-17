53f53db407aaa77a50645ed9491ff1d6d96e1dfa
james.macdonald <james.macdonald@heanet.ie>
2025-12-17 09:37:43 +0000

    quote flake

--- scripts/cm4-builder.sh
+++ scripts/cm4-builder.sh
@@ -3,7 +3,7 @@
 
 for host in tpi01 tpi02 tpi03; do
   echo "Building $host..."
-  nix build .#nixosConfigurations.$host.config.system.build.toplevel
+  nix build '.#nixosConfigurations.$host.config.system.build.toplevel'
   sleep 5s
   zstd -d ./result/sd-image/nixos-sd-image-rpi4-uboot.img.zst -o $host.img
   scp $host.img root@turingpi.lan:/mnt/sdcard/$host.img
