472f81b9127867a83617397617f24a076b61c6f5
jDmacD <jmacdoo@gmail.com>
2025-12-17 00:01:41 +0000

    disable disk config

--- nix/hosts/tpi04/configuration.nix
+++ nix/hosts/tpi04/configuration.nix
@@ -7,17 +7,17 @@
 }:
 {
   imports = [
-    ./disk-configuration.nix
+    # ./disk-configuration.nix
   ]
   ++ (with inputs.self.nixosModules; [
     rpi-common
-    k3s-agent
-    disko
+    # k3s-agent
+    # disko
   ]);
 
-  environment.etc."disko/configuration.nix" = {
-    text = builtins.readFile ./disk-configuration.nix;
-  };
+  # environment.etc."disko/configuration.nix" = {
+  #   text = builtins.readFile ./disk-configuration.nix;
+  # };
 
   networking.hostName = "tpi04";
   system.stateVersion = "24.11"; # Did you read the comment?
