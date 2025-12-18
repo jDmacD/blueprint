86a4698b7e69de2ed9c51629680d9fa9e7166b84
james.macdonald <james.macdonald@heanet.ie>
2025-12-18 17:28:32 +0000

    add user and add to trusted users

--- nix/modules/nixos/github-runner.nix
+++ nix/modules/nixos/github-runner.nix
@@ -8,6 +8,8 @@
       picard = {
         enable = true;
         name = "picard";
+        user = "githubrunner";
+        group = "githubrunner";
         tokenFile = "/run/secrets/github/token";
         url = "https://github.com/jDmacD/blueprint-ci";
         extraPackages = [
@@ -17,4 +19,16 @@
       };
     };
   };
+
+  users.users.githubrunner = {
+    isNormalUser = true;
+    createHome = false;
+    ignoreShellProgramCheck = true;
+    group = "githubrunner";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDnim/f3xwmFw/DB9zeHtQSr9i2uKxwsiXkEgE2FdFcY root@picard"
+    ];
+  };
+  users.groups.githubrunner = { };
+  nix.settings.trusted-users = [ "githubrunner" ];
 }
