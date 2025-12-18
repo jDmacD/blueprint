a080b9013cc4ed3482e7c72bcc9a815e32a9e4f9
jDmacD <jmacdoo@gmail.com>
2025-12-18 20:53:50 +0000

    make paswwordless sudo

--- nix/modules/nixos/github-runner-user.nix
+++ nix/modules/nixos/github-runner-user.nix
@@ -6,10 +6,14 @@
     createHome = true;
     ignoreShellProgramCheck = true;
     group = "githubrunner";
+    extraGroups = [
+      "wheel"
+    ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH/+YL3IpUNE+6Y/OzZ76adq953Mlpt7nGCLU4pZ0OiX"
     ];
   };
   users.groups.githubrunner = { };
+  security.sudo.wheelNeedsPassword = false;
   nix.settings.trusted-users = [ "githubrunner" ];
 }
