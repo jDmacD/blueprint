19a6f1d5830529a52722584fa3ce2f07a7bb2464
jDmacD <jmacdoo@gmail.com>
2025-12-18 21:19:45 +0000

    use extraGroups only

--- nix/modules/nixos/github-runner-user.nix
+++ nix/modules/nixos/github-runner-user.nix
@@ -5,9 +5,9 @@
     isNormalUser = true;
     createHome = true;
     ignoreShellProgramCheck = true;
-    group = "githubrunner";
     extraGroups = [
       "wheel"
+      "githubrunner"
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH/+YL3IpUNE+6Y/OzZ76adq953Mlpt7nGCLU4pZ0OiX"
