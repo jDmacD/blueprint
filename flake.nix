73df50a94bcca88883ea2517a9b030b330511377
jDmacD <jmacdoo@gmail.com>
2025-12-20 03:28:56 +0000

    picard is not remote

--- flake.nix
+++ flake.nix
@@ -118,13 +118,7 @@
             picard = mkNode {
               name = "picard";
               arch = "x86_64-linux";
-              remoteBuild = true;
             };
-            #lore = mkNode {
-            #  name = "lore";
-            #  arch = "aarch64-darwin";
-            #  remoteBuild = true;
-            #};
             worf = mkNode {
               name = "worf";
               hostname = "worf.jtec.xyz";
