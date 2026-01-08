06dc71f031c41c9c08d85087a980fbe82199db6d
jDmacD <jmacdoo@gmail.com>
2026-01-08 07:02:55 +0000

    do not deploy to surface

--- flake.nix
+++ flake.nix
@@ -146,10 +146,10 @@
               hostname = "worf.jtec.xyz";
               remoteBuild = true;
             };
-            surface = mkNode {
-              name = "surface";
-              arch = "x86_64-linux";
-            };
+            # surface = mkNode {
+            #   name = "surface";
+            #   arch = "x86_64-linux";
+            # };
             # uconsole = mkNode { name = "uconsole"; };
             pi01 = mkNode { name = "pi01"; };
             pi02 = mkNode { name = "pi02"; };
