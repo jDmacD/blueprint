80cdabec24909c44426f74e0332ede1a9b00ae55
jDmacD <jmacdoo@gmail.com>
2026-01-12 16:04:27 +0000

    add bookmarks

--- nix/modules/home/home-shared.nix
+++ nix/modules/home/home-shared.nix
@@ -23,4 +23,73 @@
       # you can access the host configuration using osConfig.
       pkgs.lib.optionals (osConfig.programs.vim.enable && pkgs.stdenv.isDarwin) [ skhd ]
     );
+
+  home.file = {
+    ".local/share/applications/shutdown.desktop".text = ''
+      [Desktop Entry]
+      Version=1.0
+      Name=Shutdown
+      Comment=Shutdown now
+      Exec=bash -c 'sudo shutdown -h now'
+      Icon=utilities-terminal
+      Terminal=true
+      Type=Application
+      Categories=Application;
+    '';
+    ".local/share/applications/reboot.desktop".text = ''
+      [Desktop Entry]
+      Version=1.0
+      Name=Reboot
+      Comment=Reboot now
+      Exec=bash -c 'sudo reboot'
+      Icon=utilities-terminal
+      Terminal=true
+      Type=Application
+      Categories=Application;
+    '';
+    ".local/share/applications/zellij.desktop".text = ''
+      [Desktop Entry]
+      Version=1.0
+      Name=Zellij
+      Comment=Open Terminal with Zellij
+      Exec=${pkgs.zellij}/bin/zellij attach -c perma-session
+      Icon=utilities-terminal
+      Terminal=true
+      Type=Application
+      Categories=Application;
+    '';
+    ".local/share/applications/nixos-packages.desktop".text = ''
+      [Desktop Entry]
+      Version=1.0
+      Name=Packages
+      Comment=NixOS Packages
+      Exec=xdg-open "https://search.nixos.org/packages"
+      Icon=utilities-terminal
+      Terminal=false
+      Type=Application
+      Categories=Application;
+    '';
+    ".local/share/applications/nixos-options.desktop".text = ''
+      [Desktop Entry]
+      Version=1.0
+      Name=Options
+      Comment=NixOS Options
+      Exec=xdg-open "https://search.nixos.org/options"
+      Icon=utilities-terminal
+      Terminal=false
+      Type=Application
+      Categories=Application;
+    '';
+    ".local/share/applications/home-manager-options.desktop".text = ''
+      [Desktop Entry]
+      Version=1.0
+      Name=Home Manager
+      Comment=Home Manager Options
+      Exec=xdg-open "https://home-manager-options.extranix.com"
+      Icon=utilities-terminal
+      Terminal=false
+      Type=Application
+      Categories=Application;
+    '';
+  };
 }
