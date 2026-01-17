faa600db3f038157df8fe1ef4084f9c02f981019
jDmacD <jmacdoo@gmail.com>
2026-01-17 02:55:40 +0000

    various improvements

--- nix/modules/home/firefox.nix
+++ nix/modules/home/firefox.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   perSystem,
   ...
 }:
@@ -8,9 +9,58 @@
     profiles = {
       jmacdonald = {
         extensions = {
+          force = true;
           packages = with perSystem.nur.repos.rycee.firefox-addons; [
             bitwarden
           ];
+        };
+        settings = {
+          "signon.rememberSignons" = false;
+          "browser.startup.homepage" = "https://search.jtec.xyz";
+        };
+
+        search = {
+          force = true;
+          default = "Searx";
+          order = [
+            "searx"
+            "google"
+          ];
+          engines = {
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@np" ];
+            };
+            "NixOS Wiki" = {
+              urls = [ { template = "https://nixos.wiki/index.php?search={searchTerms}"; } ];
+              icon = "https://nixos.wiki/favicon.png";
+              updateInterval = 24 * 60 * 60 * 1000; # every day
+              definedAliases = [ "@nw" ];
+            };
+            "Searx" = {
+              urls = [ { template = "https://search.jtec.xyz/?q={searchTerms}"; } ];
+              icon = "https://nixos.wiki/favicon.png";
+              updateInterval = 24 * 60 * 60 * 1000; # every day
+              definedAliases = [ "@searx" ];
+            };
+            "bing".metaData.hidden = true;
+            "google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+          };
         };
         bookmarks = {
           force = true;
@@ -19,22 +69,6 @@
               name = "Nix sites";
               toolbar = true;
               bookmarks = [
-                {
-                  name = "Home Manager Options";
-                  url = "https://home-manager-options.extranix.com/?query=&release=release-25.11";
-                }
-                {
-                  name = "Stylix";
-                  url = "https://nix-community.github.io/stylix/";
-                }
-                {
-                  name = "NixOS Packages";
-                  url = "https://search.nixos.org/packages?channel=25.11&query=";
-                }
-                {
-                  name = "Numtide Blueprint";
-                  url = "https://numtide.github.io/blueprint/main/";
-                }
               ];
             }
           ];
