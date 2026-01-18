83468deacaf0f82a3c8d9a99b385a39c19729cf3
jDmacD <jmacdoo@gmail.com>
2026-01-18 11:47:08 +0000

    add monitor settings

--- nix/hosts/lwh-hotapril/users/jmacdonald/home-configuration.nix
+++ nix/hosts/lwh-hotapril/users/jmacdonald/home-configuration.nix
@@ -24,5 +24,17 @@
 
   programs.claude-code.preset = "work";
 
+  programs.noctalia-shell.wallpaper.monitorDirectories = [
+    {
+      name = "DP-1";
+      dimensions = "3840x1600";
+      wallpaper = "wallhaven-gwweme_3840x1600.png";
+    }
+    {
+      name = "eDP-1";
+      dimensions = "1920x1080";
+      wallpaper = "wallhaven-0q2877_1920x1080.png";
+    }
+  ];
   home.stateVersion = "25.11";
 }
