270b1208fe06b4e26039604e5bd78180b5952a15
jDmacD <jmacdoo@gmail.com>
2026-01-20 18:13:32 +0000

    add stuff to vscode

--- nix/modules/home/vscode.nix
+++ nix/modules/home/vscode.nix
@@ -29,6 +29,11 @@ let
 in
 {
 
+  home.packages = with pkgs; [
+    mermaid-cli
+    mermaid-filter
+  ];
+
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
@@ -51,6 +56,14 @@ in
           chat.showAgentSessionsViewDescription = false;
           terminal.integrated.initialHint = false;
           sops.defaults.ageKeyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+
+          markdown-preview-enhanced.pandocPath = "${pkgs.pandoc}/bin/pandoc";
+          markdown-preview-enhanced.latexEngine = "${pkgs.texliveFull}/bin/xelatex";
+          markdown-preview-enhanced.imageMagickPath = "${pkgs.imagemagick}/bin/magick";
+          markdown-preview-enhanced.chromePath = "${pkgs.ungoogled-chromium}/bin/chromium";
+          markdown-preview-enhanced.usePandocParser = true;
+          markdown-preview-enhanced.krokiServer = "https://kroki.heanet.io";
+          cSpell.language = "en-GB";
         };
         extensions =
           with pkgs.vscode-extensions;
@@ -63,6 +76,8 @@ in
             ms-azuretools.vscode-docker
             mechatroner.rainbow-csv
             signageos.signageos-vscode-sops
+            streetsidesoftware.code-spell-checker
+            shd101wyy.markdown-preview-enhanced
             # anthropic.claude-code
             /*
               Continue extension requires platform-specific builds:
