a1121a3f7746f68ef2d647365703f682fa4cddbc
jDmacD <jmacdoo@gmail.com>
2026-01-21 00:56:02 +0000

    update deploy scripts

--- nix/packages/fleet-deploy/default.nix
+++ nix/packages/fleet-deploy/default.nix
@@ -6,9 +6,11 @@ pkgs.writeShellScriptBin "fleet-deploy" ''
   export PATH="${pkgs.lib.makeBinPath [ pkgs.openssh ]}:$PATH"
 
   ${pkgs.deploy-rs}/bin/deploy \
+    --skip-checks \
     --ssh-user githubrunner \
     --activation-timeout 300 \
     --ssh-opts="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /run/secrets/githubrunner/githubrunner_ed25519" \
+    --targets .#pi01 .#pi02 .#pi03 .#pi04 .#pi05 .#tpi04 .#tpi03 .#tpi02 .#tpi01 .#picard
     . \
     -- \
       --accept-flake-config \
