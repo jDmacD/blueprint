33631287ee731a47673726c789a78e867872c91a
jDmacD <jmacdoo@gmail.com>
2026-01-23 16:35:47 +0000

    use stylix directly

--- nix/modules/nixos/stylix.nix
+++ nix/modules/nixos/stylix.nix
@@ -1,10 +1,50 @@
 { pkgs, inputs, ... }:
+let
+  theme = "material-vivid";
+in
 {
 
   imports = [
     inputs.stylix.nixosModules.stylix
   ];
 
-  stylix = import inputs.self.lib.stylix { inherit pkgs inputs; };
+  stylix = {
+    enable = true;
+    autoEnable = true;
+    image = (import inputs.self.lib.wallpapers { inherit pkgs; }).moonRise;
+    targets = { };
 
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/${theme}.yaml";
+    polarity = "dark";
+    fonts = {
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+
+      monospace = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans Mono";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-color-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
+    opacity = {
+      terminal = 0.7;
+    };
+
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Classic";
+      size = 24;
+    };
+  };
 }
