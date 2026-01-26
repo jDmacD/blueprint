8849e7e12574b5a3054c615149116e94a3d0a6e2
jDmacD <jmacdoo@gmail.com>
2026-01-26 23:32:56 +0000

    crate sunshine app

--- nix/modules/nixos/sunshine.nix
+++ nix/modules/nixos/sunshine.nix
@@ -1,4 +1,7 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  hyprctl = "${pkgs.hyprland}/bin/hyprctl --instance 0";
+in
 {
   services.sunshine = {
     enable = true;
@@ -7,6 +10,33 @@
     openFirewall = true;
     settings = {
       capture = "wlr";
+    };
+    # https://gist.github.com/Dregu/4c0dbb2582835e5d95e06c4bf7624e3b`
+    applications = {
+      apps = [
+        {
+          name = "1920x1080 Steam";
+          prep-cmd = [
+            {
+              do = "${hyprctl} output create headless SUNSHINE-1";
+              undo = "${hyprctl} output remove SUNSHINE-1";
+            }
+            {
+              do = ''
+                sh -c "${hyprctl} keyword monitor SUNSHINE-1,''${SUNSHINE_CLIENT_WIDTH}x''${SUNSHINE_CLIENT_HEIGHT}@''${SUNSHINE_CLIENT_FPS},auto,1"
+              '';
+              undo = "${hyprctl} keyword monitor SUNSHINE-1,disable";
+            }
+            {
+              do = "${hyprctl} keyword monitor DP-1,disable";
+              undo = "${hyprctl} reload";
+            }
+          ];
+          exclude-global-prep-cmd = "false";
+          auto-detach = "true";
+        }
+      ];
+
     };
   };
 
