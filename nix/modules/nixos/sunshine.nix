7ffbeb729e1f8f43dec3182358eb877e178ac4b7
jDmacD <jmacdoo@gmail.com>
2026-01-27 02:32:57 +0000

    formatting

--- nix/modules/nixos/sunshine.nix
+++ nix/modules/nixos/sunshine.nix
@@ -22,12 +22,20 @@ in
             #   undo = "${hyprctl} output remove SUNSHINE-1";
             # }
             {
-              do = "${pkgs.bash}/bin/bash -c 'export HYPRLAND_INSTANCE_SIGNATURE=$(ls -t $XDG_RUNTIME_DIR/hypr | head -1) && ${hyprctl} keyword monitor SUNSHINE-1,''${SUNSHINE_CLIENT_WIDTH}x''${SUNSHINE_CLIENT_HEIGHT}@''${SUNSHINE_CLIENT_FPS},auto,1'";
-              undo = "${pkgs.bash}/bin/bash -c 'export HYPRLAND_INSTANCE_SIGNATURE=$(ls -t $XDG_RUNTIME_DIR/hypr | head -1) && ${hyprctl} keyword monitor SUNSHINE-1,disable'";
+              do = ''
+                ${pkgs.bash}/bin/bash -c "export HYPRLAND_INSTANCE_SIGNATURE=$(ls -t $XDG_RUNTIME_DIR/hypr | head -1) && ${hyprctl} keyword monitor SUNSHINE-1,''${SUNSHINE_CLIENT_WIDTH}x''${SUNSHINE_CLIENT_HEIGHT}@''${SUNSHINE_CLIENT_FPS},auto,1"
+              '';
+              undo = ''
+                ${pkgs.bash}/bin/bash -c "export HYPRLAND_INSTANCE_SIGNATURE=$(ls -t $XDG_RUNTIME_DIR/hypr | head -1) && ${hyprctl} keyword monitor SUNSHINE-1,disable"
+              '';
             }
             {
-              do = "${pkgs.bash}/bin/bash -c 'export HYPRLAND_INSTANCE_SIGNATURE=$(ls -t $XDG_RUNTIME_DIR/hypr | head -1) && ${hyprctl} keyword monitor DP-1,disable'";
-              undo = "${pkgs.bash}/bin/bash -c 'export HYPRLAND_INSTANCE_SIGNATURE=$(ls -t $XDG_RUNTIME_DIR/hypr | head -1) && ${hyprctl} reload'";
+              do = ''
+                ${pkgs.bash}/bin/bash -c "export HYPRLAND_INSTANCE_SIGNATURE=$(ls -t $XDG_RUNTIME_DIR/hypr | head -1) && ${hyprctl} keyword monitor DP-1,disable"
+              '';
+              undo = ''
+                ${pkgs.bash}/bin/bash -c "export HYPRLAND_INSTANCE_SIGNATURE=$(ls -t $XDG_RUNTIME_DIR/hypr | head -1) && ${hyprctl} reload"
+              '';
             }
           ];
           exclude-global-prep-cmd = "false";
