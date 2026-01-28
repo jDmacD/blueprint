dfc0d9c67ba38b4b42d36b85922da6b152b28816
jDmacD <jmacdoo@gmail.com>
2026-01-28 11:47:47 +0000

    override greetd

--- nix/lib/greetd.nix
+++ nix/lib/greetd.nix
@@ -1,6 +1,15 @@
 { flake, inputs, ... }:
+{ pkgs, ... }:
 {
   imports = [
     ../modules/nixos/greetd.nix
   ];
+
+  # Override to use hyprstart from THIS flake (not consumer's flake)
+  services.greetd.settings = {
+    initial_session.command = "${flake.packages.${pkgs.system}.hyprstart}";
+    default_session.command =
+      let tuigreet = "${pkgs.tuigreet}/bin/tuigreet";
+      in "${tuigreet} --greeting 'Welcome to NixOS!' --asterisks --remember --remember-user-session --time -c ${flake.packages.${pkgs.system}.hyprstart}";
+  };
 }
--- nix/modules/nixos/greetd.nix
+++ nix/modules/nixos/greetd.nix
@@ -1,10 +1,8 @@
-{ flake, inputs, ... }:  # ← Outer function: Blueprint calls this with YOUR flake
-{ pkgs, perSystem, ... }:  # ← Inner function: The actual NixOS module
+{ pkgs, perSystem, ... }:
 
 let
   tuigreet = "${pkgs.tuigreet}/bin/tuigreet";
-  # Use flake from outer scope - always refers to THIS flake
-  session = "${flake.packages.${pkgs.system}.hyprstart}";
+  session = perSystem.self.hyprstart;
   username = "jmacdonald";
 in
 
@@ -13,8 +11,8 @@ in
     enable = true;
     settings = {
       initial_session = {
-        command = session;
-        user = username;
+        command = "${session}";
+        user = "${username}";
       };
       default_session = {
         command = "${tuigreet} --greeting 'Welcome to NixOS!' --asterisks --remember --remember-user-session --time -c ${session}";
