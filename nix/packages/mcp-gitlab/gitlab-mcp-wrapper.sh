ba05344079f383d7cbb2462a6129b313e061e773
James MacDonald <jmacdonald@jtec.xyz>
2026-04-13 10:41:27 +0100

    Change GITLAB_READ_ONLY_MODE to false

--- nix/packages/mcp-gitlab/gitlab-mcp-wrapper.sh
+++ nix/packages/mcp-gitlab/gitlab-mcp-wrapper.sh
@@ -5,7 +5,7 @@ export GITLAB_HOST=git.heanet.ie
 
 export GITLAB_PERSONAL_ACCESS_TOKEN=$(yq '.hosts.[env(GITLAB_HOST)].token' ${HOME}/.config/glab-cli/config.yml)
 export GITLAB_API_URL=https://${GITLAB_HOST}/api/v4
-export GITLAB_READ_ONLY_MODE=true
+export GITLAB_READ_ONLY_MODE=false
 export USE_PIPELINE=false
 
 # Run the MCP server
