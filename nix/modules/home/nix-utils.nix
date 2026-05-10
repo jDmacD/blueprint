c3ebd27e25fdc8fa12cd7094a3342108e58ba3e6
jDmacD <jmacdoo@gmail.com>
2026-05-11 00:18:24 +0100

    enable formatter

--- nix/modules/home/nix-utils.nix
+++ nix/modules/home/nix-utils.nix
@@ -1,13 +1,16 @@
 { pkgs, config, ... }:
 {
 
-  home.packages = with pkgs; [
-    cachix
-    deploy-rs
-    nixos-rebuild-ng
-    nixos-anywhere
-    # disko
-  ];
+  home.packages =
+    with pkgs;
+    [
+      cachix
+      deploy-rs
+      nixos-rebuild-ng
+      nixos-anywhere
+      # disko
+    ]
+    ++ (with perSystem.self; [ cachix-update ]);
 
   programs.nh = {
     enable = true;
